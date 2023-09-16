1b83b303190d1257ed5a52a62e859b57c83cb712
CyborgBunni <100697901+CyborgBunni@users.noreply.github.com>
2023-09-16 13:08:26 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/index.css 	modified:   src/index.js 	public/favicon.ico.png 	public/sugarbackground.jpg 	src/CashApp.js 	src/Confirmation.js 	src/Header.js 	src/Home.js 	src/PayPal.js 	src/Venmo.js 	src/logo.gif

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { 
+  BrowserRouter,
+  Routes, 
+  Route  
+} from 'react-router-dom';
+import Home from './Home';
+import CashApp from './CashApp';
+import PayPal from './PayPal';
+import Venmo from './Venmo';
+import Header from './Header';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+          <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cashapp" element={<CashApp />} />
+          <Route path="/PayPal" element={<PayPal />} />
+          <Route path="/Venmo" element={<Venmo />} />
+        </Routes>
+      </BrowserRouter>
   );
-}
-
-export default App;
+};
